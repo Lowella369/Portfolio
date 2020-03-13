c5aa545dc4978f378652d9a56944a9fe5ae79546
Lowella369 <lh850210@gmail.com>
2020-03-13 16:51:22 -0500

    Modified script.js

--- script/script.js
+++ script/script.js
@@ -1,10 +1,6 @@
-const footerTemplate = `
-  <div class="connect">
-    <a href="https://github.com/Lowella369" class="github" target="_blank">GitHub</a>
-    <a href="https://www.linkedin.com/in/lowella-fox-276347bb/" class="linkedin" target="_blank">LinkedIn</a>
-  </div>
-  <p>Copyright Lowella Fox 2020</p>
-  <p><a href="html/contact.html">Contact</a></p>
-  `;
+const footerTemplate = "<a href='https://github.com/Lowella369' class='github' target='_blank'>GitHub</a>"
+	+ "<a href='https://www.linkedin.com/in/lowella-fox-276347bb/' class='linkedin' target='_blank'>LinkedIn</a>"
+	+ "<p>Copyright Lowella Fox 2020</p>"
+  + "<p><a href='html/contact.html'>Contact</a></p>";
 
-document.querySelector( '.js-footer' ).innerHTML = footerTemplate;
+document.getElementById("js-footer").innerHTML =footerTemplate;
