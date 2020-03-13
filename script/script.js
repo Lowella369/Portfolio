dd850f97b402d5bb719a4de66af7aba7f18faa51
Lowella369 <lh850210@gmail.com>
2020-03-13 15:19:23 -0500

    Adding script for the footer template

---
+++ script/script.js
@@ -0,0 +1,10 @@
+const footerTemplate = `
+  <div class="connect">
+    <a href="https://github.com/Lowella369" class="github" target="_blank">GitHub</a>
+    <a href="https://www.linkedin.com/in/lowella-fox-276347bb/" class="linkedin" target="_blank">LinkedIn</a>
+  </div>
+  <p>Copyright Lowella Fox 2020</p>
+  <p><a href="html/contact.html">Contact</a></p>
+  `;
+
+document.querySelector( '.js-footer' ).innerHTML = footerTemplate;
